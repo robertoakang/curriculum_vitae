0e16d50de6e54d795fc3815e2de21275aeec6452
zKatsumi <betoakang@gmail.com>
2019-12-27 17:05:39 -0300

    Day 2

--- website/project-cv/src/sections/Home.js
+++ website/project-cv/src/sections/Home.js
@@ -16,16 +16,20 @@ class Home extends Component {
                         <div className="home-section-center-info">
                             <div className="home-section-center-info-text">
                                 <p style={{fontFamily: "'Forum', cursive", fontWeight: "bold", letterSpacing: "1px"}}>Roberto Arruda Kang</p>
-                                <p style={{fontSize: "0.8em", marginTop: "5px", letterSpacing: "1.5px"}}>DESENVOLVEDOR FRONT-END</p>
+                                <p style={{fontSize: "0.8em", marginTop: "5px", letterSpacing: "1.5px"}}>DESENVOLVEDOR FRONT-END/FULLSTACK</p>
 
-                                <p style={{fontSize: "0.7em", marginTop: "25px", fontWeight: "bold"}}>Telefone:</p>
-                                <p style={{fontSize: "0.7em", marginTop: "5px", fontWeight: "500"}}>(11) 94935-3512</p>
+                                <p style={{fontSize: "0.7em", marginTop: "25px", fontWeight: "bold"}}>Formação:</p>
+                                <p style={{fontSize: "0.7em", marginTop: "5px", fontWeight: "500"}}>• Curso de Web Developer - Microcamp/Evolutime (2015-2017)</p>
+                                <p style={{fontSize: "0.7em", marginTop: "5px", fontWeight: "500"}}>• Curso Superior de S.I - UNIP (2017/1)</p>
+
+                                <p style={{fontSize: "0.7em", marginTop: "15px", fontWeight: "bold"}}>Tecnologias preferidas:</p>
+                                <p style={{fontSize: "0.7em", marginTop: "5px", fontWeight: "500"}}>React, React Native e NodeJS</p>
 
                                 <p style={{fontSize: "0.7em", marginTop: "15px", fontWeight: "bold"}}>Email:</p>
-                                <p style={{fontSize: "0.7em", marginTop: "5px", fontWeight: "500"}}>betoakang@hotmail.com</p>
-                                
-                                <p style={{fontSize: "0.7em", marginTop: "15px", fontWeight: "bold"}}>Email:</p>
-                                <p style={{fontSize: "0.7em", marginTop: "5px", fontWeight: "500"}}>betoakang@hotmail.com</p>
+                                <p style={{fontSize: "0.7em", marginTop: "5px", fontWeight: "500"}}>betoakang@gmail.com</p>
+
+                                <p style={{fontSize: "0.7em", marginTop: "15px", fontWeight: "bold"}}>Data de nascimento:</p>
+                                <p style={{fontSize: "0.7em", marginTop: "5px", fontWeight: "500"}}>01/06/2000</p>
                             </div>
                         </div>
                         <div className="home-section-center-footer">
@@ -34,6 +38,12 @@ class Home extends Component {
                             <p><a href="https://github.com/robertoakang" target="_blank"><img src={facebook} class="home-section-center-socials" /></a></p>
                             <p><a href="https://github.com/robertoakang" target="_blank"><img src={instagram} class="home-section-center-socials" /></a></p>
                         </div>
+                    </div>
+                    <div className="home-section-footer">
+                        <h2>Um pouco sobre mim</h2>
+                        <p style={{marginTop: "10px"}}>Apaixonado por tecnologia e todo seu ecossistema.</p>
+                        <p>Nascido no meio desta revolução tecnológica, aprendi a amar e usar a tecnologia ao meu favor, seja </p>
+                        <p>ela automatizando processos ou facilitando a vida das pessoas. A tecnologia é a vida atual do ser humano!</p>
                     </div>
                 </div>
             </div>
